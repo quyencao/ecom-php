10d44fb70af2302e7dce4fff2584aedf5fbe3b83
quyencao <boy20195@gmail.com>
2017-03-09 18:45:13 +0700

    Add constant for resources

--- resources/config.php
+++ resources/config.php
@@ -2,4 +2,8 @@
 
     defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
-    echo __DIR__;
+    defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
+
+    defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
+
+    echo TEMPLATE_BACK;
